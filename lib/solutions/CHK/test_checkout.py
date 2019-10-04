79451a772cb1e3108ac71b398dad6bc0c2085b56
sbx_user1051 <sbx_user1051@169.254.97.21>
2019-10-04 10:08:45 +0000

    Fri Oct 04 10:08:45 UTC 2019

--- lib/solutions/CHK/test_checkout.py
+++ lib/solutions/CHK/test_checkout.py
@@ -1,9 +1,11 @@
 import unittest
-import checkout_solutions
+import checkout_solution
 
 class TestCheckout(unittest.TestCase):
 
     def test_checkout(self):
-        self.assertEqual(checkout_solutions.chechout("AABBEE"),195)
+        self.assertEqual(checkout_solution.checkout("AABBEE"),195)
+        self.assertEqual(checkout_solution.checkout("ABCDEGX"),-1)
+        self.assertEqual(checkout_solution.checkout("AAAAAA"),250)
 if __name__ == "__main__":
     unittest.main()
