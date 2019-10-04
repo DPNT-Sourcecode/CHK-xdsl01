76ab7989e27857c41a792601d107bac09111d118
sbx_user1051 <sbx_user1051@169.254.97.21>
2019-10-04 08:30:33 +0000

    Fri Oct 04 08:30:33 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,13 +3,22 @@ from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+
+    pricing = {"A":50,
+                "B":30,
+                "C":20,
+                "D":15}
+    discounts = {"A":0,
+                "B":0}
+
     items_counter = Counter(skus)
 
     for item in items_counter:
         quantity_of_item = items_counter[item]
-        print(item,quantity_of_item)
+        
     
 
 checkout("AAABC")
+
 
 
