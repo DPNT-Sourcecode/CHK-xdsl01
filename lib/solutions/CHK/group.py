183eb62a5bdc835139eb09fb983ff359ed304330
sbx_user1051 <sbx_user1051@169.254.97.21>
2019-10-04 14:40:06 +0000

    Fri Oct 04 14:40:06 UTC 2019

--- lib/solutions/CHK/group.py
+++ lib/solutions/CHK/group.py
@@ -1,3 +1,30 @@
 import json
+from collections import OrderedDict
 with open("./lib/solutions/CHK/stock.json",'r') as f:
     stock = json.load(f)
+
+stock_lookup = {}
+for index,product in enumerate(stock):
+    stock_lookup[product['item']] = index
+
+def rplace_group(s):
+    qualifying_letters = []
+    for letter in s:
+        if letter in ("S","T","X","Y","Z"):
+            qualifying_letters.append(letter)
+    
+    if len(qualifying_letters) < 3:
+        return s
+    
+    group_discounts_allowed = int(len(qualifying_letters)/3)
+
+    od = {}
+    for letter in qualifying_letters:
+        od[stock[stock_lookup[letter]]["item"]] = stock[stock_lookup[letter]]["core_price"]
+    
+    sorted_od = OrderedDict(od)
+
+
+
+
+
