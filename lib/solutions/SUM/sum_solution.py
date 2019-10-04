2b824b7b5b3be12f3a0f979d883b0f5611fb2f2d
sbx_user1051 <sbx_user1051@169.254.97.21>
2019-10-04 08:08:51 +0000

    Fri Oct 04 08:08:51 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -5,13 +5,12 @@
  - param[1] = a positive integer between 0-100
  - @return = an Integer representing the sum of the two numbers"""
 def compute(x, y):
+    #
     if not all([isinstance(x,int),isinstance(y,int)]):
         raise AttributeError("Both args must be an int")
-    if x > 100 or y > 100:
+    if x not in range(0,101) or y not in range(0,101):
         raise AttributeError("Both args must be between 0 and 100")
     return x + y
     
 
-print(compute(-2,10))
-
-
+print(compute(0,140))
