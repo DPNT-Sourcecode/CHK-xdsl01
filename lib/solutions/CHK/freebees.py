b770e1afa2c569fe3403789183872d7cf580f88c
sbx_user1051 <sbx_user1051@169.254.97.21>
2019-10-04 11:20:32 +0000

    Fri Oct 04 11:20:32 UTC 2019

--- lib/solutions/CHK/freebees.py
+++ lib/solutions/CHK/freebees.py
@@ -4,33 +4,31 @@ freebee_list = [{"letter_needed":"E","quantity_needed":2,"letter_removal":"B"}]
 freebee_lookup = {}
 for index,product in enumerate(freebee_list):
     freebee_lookup[product['letter_needed']] = index
+
 def apply_freebees(skus):
     item_counter = dict(Counter(skus))
-    print(item_counter)
+    
     for k,v in item_counter.items():
         if k not in freebee_lookup.keys():
             continue
         
         possible_discounts = int(v/freebee_list[freebee_lookup[k]]["quantity_needed"])
-        print(possible_discounts)
         letter_that_can_be_removed = freebee_list[freebee_lookup[k]]["letter_removal"]
         items_that_can_be_removed = int(possible_discounts/item_counter.get(letter_that_can_be_removed,possible_discounts+1))
-        print(items_that_can_be_removed)
-
-        if possible_discounts >= items_that_can_be_removed:
+        
+        while item_counter[letter_that_can_be_removed] > 0:
             for discount in range(possible_discounts):
-                item_counter[letter_that_can_be_removed] - 1
+                item_counter[letter_that_can_be_removed] -= 1
 
-        # if items_that_can_be_removed > 0:
-        #     item_counter[letter_that_can_be_removed] -= items_that_can_be_removed
-
+    #Rebuild the string with the items taken away
     return_string = ""
     for k,v in item_counter.items():
         for i in range(v):
             return_string = return_string+k
     print(return_string)
 
-apply_freebees("EEEEBBD")
+apply_freebees("EEEEEEEBBBBBBBD")
+
 
 
 
