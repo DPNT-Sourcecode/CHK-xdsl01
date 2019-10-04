a0eaee28b874e5f41b9190046d7d28ea2297eda4
sbx_user1051 <sbx_user1051@169.254.97.21>
2019-10-04 08:36:33 +0000

    Fri Oct 04 08:36:33 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,17 +8,30 @@ def checkout(skus):
                 "B":30,
                 "C":20,
                 "D":15}
-    discounts = {"A":0,
-                "B":0}
+    discounts = ("A","B")
 
     items_counter = Counter(skus)
 
+    price = 0
+
     for item in items_counter:
         quantity_of_item = items_counter[item]
+
+        if item not in discounts:
+            price += pricing[item] * quantity_of_item
+            continue
         
+        if item == "A" and quantity_of_item >= 3:
+            
+        else:
+            price += pricing[item] * quantity_of_item
     
 
-checkout("AAABC")
+
+    
+
+checkout("AAABCD")
+
 
 
 
