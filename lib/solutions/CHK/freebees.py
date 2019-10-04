3445e902eedb7b6f94289f6eed552a427cc2af04
sbx_user1051 <sbx_user1051@169.254.97.21>
2019-10-04 11:05:32 +0000

    Fri Oct 04 11:05:32 UTC 2019

--- lib/solutions/CHK/freebees.py
+++ lib/solutions/CHK/freebees.py
@@ -3,18 +3,27 @@ from collections import Counter
 freebee_list = [{"letter_needed":"E","quantity_needed":2,"letter_removal":"B"}]
 freebee_lookup = {}
 for index,product in enumerate(freebee_list):
-    freebee_lookup[product['item']] = index
-
+    freebee_lookup[product['letter_needed']] = index
+print(freebee_lookup)
 def apply_freebees(skus):
     item_counter = dict(Counter(skus))
-
+    print(item_counter)
     for k,v in item_counter.items():
         if not freebee_lookup.get(k,None):
             continue
         
         possible_discounts = int(v/freebee_list[freebee_lookup[k]]["quantity_needed"])
+        print(possible_discounts)
         letter_that_can_be_removed = freebee_list[freebee_lookup[k]]["letter_removal"]
         items_that_can_be_removed = int(possible_discounts/item_counter.get(letter_that_can_be_removed,possible_discounts+1))
+        print(items_that_can_be_removed)
+        if items_that_can_be_removed > 0:
+            item_counter[letter_that_can_be_removed] -= items_that_can_be_removed
+
+    print(item_counter)
+
+apply_freebees("EEEEBB")
+
 
 
 
