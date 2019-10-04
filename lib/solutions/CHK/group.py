c12521dd620cf3f55cabddf4d507759a4ca75ce9
sbx_user1051 <sbx_user1051@169.254.97.21>
2019-10-04 15:01:06 +0000

    Fri Oct 04 15:01:06 UTC 2019

--- lib/solutions/CHK/group.py
+++ lib/solutions/CHK/group.py
@@ -18,6 +18,7 @@ def rplace_group(s):
     
     group_discounts_allowed = int(len(qualifying_letters)/3)
 
+    #we want to put the most expensive items into the group of 3 to make £45
     list_of_letters_and_cost =[{"item":stock[stock_lookup[letter]]['item'],
                                 "core_price":stock[stock_lookup[letter]]['core_price']} for letter in qualifying_letters]
     list_of_letters_high_to_low = sorted(list_of_letters_and_cost, key=lambda letter:letter['core_price'],reverse=True)
@@ -29,7 +30,7 @@ def rplace_group(s):
     return s
 
 
-print(rplace_group("ABCY"))
+print(rplace_group("ABCSTXZZZYY"))
 
 
 
