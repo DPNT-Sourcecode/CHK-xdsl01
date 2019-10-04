ab4d23087778164edc57e408b7ff7327d4363ea6
sbx_user1051 <sbx_user1051@169.254.97.21>
2019-10-04 07:53:51 +0000

    Fri Oct 04 07:53:51 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,11 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+
+"""Where:
+ - param[0] = a positive integer between 0-100
+ - param[1] = a positive integer between 0-100
+ - @return = an Integer representing the sum of the two numbers"""
 def compute(x, y):
-    raise NotImplementedError()
+    if not all([isinstance(x,int),isinstance(y,int)]):
+        raise AttributeError("Both args must be an int")
+    return x + y
+
