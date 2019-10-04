d31e59d40da995c50834c68e9a1fe44aa82b3742
sbx_user1051 <sbx_user1051@169.254.97.21>
2019-10-04 10:56:32 +0000

    Fri Oct 04 10:56:32 UTC 2019

--- lib/solutions/CHK/freebees.py
+++ lib/solutions/CHK/freebees.py
@@ -1,6 +1,15 @@
 from collections import Counter
 
+freebee_list = [{"letter_needed":"E","quantity_needed":2,"letter_removal":"B"}]
+freebee_lookup = {}
+for index,product in enumerate(freebee_list):
+    freebee_lookup[product['item']] = index
 def apply_freebees(skus):
     item_counter = dict(Counter(skus))
+
+    for k,v in item_counter.items():
+
+
+
 
 
