e1e8fa63589db3ecb178cdcbfa720e4782521cf8
sbx_user1051 <sbx_user1051@169.254.97.21>
2019-10-04 08:51:02 +0000

    Fri Oct 04 08:51:02 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,9 +10,9 @@ def checkout(skus):
                 "B":30,
                 "C":20,
                 "D":15}
-    discounts = {"A":3,
+    discount_quantities = {"A":3,
                 "B":2}
-    discounts_offer = {"A":130,
+    discount_offer = {"A":130,
                         "B":45}
 
     items_counter = Counter(skus)
@@ -34,12 +34,16 @@ def checkout(skus):
         if quantity_of_item < discounts[item]:
             price += pricing[item] * quantity_of_item
         else:
-            number_of_applicaple_discounts = quantity_of_item/discounts[item]
-            remaining_quantity_outside_discount = quantity_of_item-(int(number_of_applicaple_discounts)*discounts[item])
-            total_items_cost = (int(number_of_applicaple_discounts)*discounts_offer[item]) + (remaining_quantity_outside_discount*pricing[item])
+            #We need to check how many of the discounts we can apply to this item.
+            number_of_applicaple_discounts = quantity_of_item/discount_quantities[item]
+            #The remaining quantity goes at the standard price.
+            remaining_quantity_outside_discount = quantity_of_item-(int(number_of_applicaple_discounts)*discount_quantities[item])
+            #Work out the final cost for that item
+            total_items_cost = (int(number_of_applicaple_discounts)*discount_offer[item]) + (remaining_quantity_outside_discount*pricing[item])
             price += total_items_cost
         
     return price
+
 
 
 
