b4488f8b4c021cceccd5113f1d7c0cf83ad7f44f
sbx_user1051 <sbx_user1051@169.254.97.21>
2019-10-04 14:52:06 +0000

    Fri Oct 04 14:52:06 UTC 2019

--- lib/solutions/CHK/group.py
+++ lib/solutions/CHK/group.py
@@ -18,10 +18,14 @@ def rplace_group(s):
     
     group_discounts_allowed = int(len(qualifying_letters)/3)
 
-    print(group_discounts_allowed)
-
+    list_of_letters_and_cost =[{"item":stock[stock_lookup[letter]]['item'],
+                                "core_price":stock[stock_lookup[letter]]['core_price']} for letter in qualifying_letters]
+    list_of_letters_high_to_low = sorted(list_of_letters_and_cost, key=lambda letter:letter['core_price'])
+    print(list_of_letters_high_to_low)
+    print(list(list_of_letters_high_to_low))
 
 rplace_group("ZSYYY")
+
 
 
 
