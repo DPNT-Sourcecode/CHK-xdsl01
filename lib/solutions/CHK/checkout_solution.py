a973a2fecadd30fac603f363fc0585e144609085
sbx_user1051 <sbx_user1051@169.254.97.21>
2019-10-04 10:14:32 +0000

    Fri Oct 04 10:14:32 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,7 +14,7 @@ import json
 +------+-------+------------------------+"""
 
 #Load out stock from a json file, otherwise this could get massive
-with open("stock.json",'r') as f:
+with open("./lib/solutions/CHK/stock.json",'r') as f:
     stock = json.load(f)
 
 #Create a dict to allow us to quickly find the item in question
